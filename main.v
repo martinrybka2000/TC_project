2f932997a9a39ddefa57ac92ad1734a38f5bc526
martinrybka2000 <rybatinek14@gmail.com>
2022-05-20 21:17:28 +0200

    added modules elipsa and 3hz diwider

--- main.v
+++ main.v
@@ -1,7 +1,7 @@
 /*
 Adrian Mazur, Martin Rybka, grupa 7, poniedziałek 12;30, 25.04.2022
 */
-// test
+
 module main(clk_main, switch_inWombat, switch_inDanger, switch_Damaged, switch_Immobilized, LEDs);
 
     input switch_Damaged;
@@ -10,20 +10,22 @@ module main(clk_main, switch_inWombat, switch_inDanger, switch_Damaged, switch_I
     input switch_inWombat;
     input switch_inDanger;
     
-    output [3:0] LEDs;
+    output [7:0] LEDs;
     
     wire kabel_clk_1s;
     wire kabel_clk_10ms;
+    //wire kabel_clk_333ms;
     wire kabel_inWombat;
     wire kabel_inDanger;
     wire kabel_Damaged;
     wire kabel_Immobilized;
     wire kabel_I_am_fucked;
-    wire [3:0] kable_LEDs;
+    wire [7:0] kable_LEDs;
     
 
     divider_1s div_1s(clk_main, kabel_clk_1s);
     divider_10ms div_10ms(clk_main, kabel_clk_10ms);
+    //divider_333ms div_333ms(clk_main, kabel_clk_333ms);
     
     Debouncer inCombat(kabel_clk_10ms, switch_inWombat, kabel_inWombat);
     Debouncer inDanger(kabel_clk_10ms, switch_inDanger, kabel_inDanger);
@@ -34,7 +36,7 @@ module main(clk_main, switch_inWombat, switch_inDanger, switch_Damaged, switch_I
 
     amIfucked doI(kabel_clk_10ms, kabel_inDanger, kabel_Damaged, kabel_Immobilized, kabel_I_am_fucked);
 
-    dead isdead(kabel_clk_1s , kable_LEDs, LEDs);
+    epilepsy my_eyes(kabel_clk_1s, kabel_clk_333ms kable_LEDs, LEDs);
 
 endmodule
 
@@ -56,18 +58,18 @@ module amIfucked(clk, danger ,damaged, immobilized, i_m_fucked);
     
 endmodule
 
-module dead(clk, in_cnt, display);
+module epilepsy(clk, clk_3Hz, in_cnt, display);
     input clk;
-    input[3:0] in_cnt;
-    output reg[3:0] display;
+    input clk_3Hz;
+    input[7:0] in_cnt;
+    output reg[7:0] display;
 
     always @(posedge clk) begin    
-        if(in_cnt == 4'b1010) begin 
-            display <= 15; 
-        end
-        else begin
-            display <= in_cnt;
-        end
+        display <= in_cnt;
+    end
+
+    always @(posedge clk_3Hz) begin
+        display = (8'b11111111 ^ display) & in_cnt; 
     end
 endmodule
 
@@ -97,7 +99,24 @@ module divider_10ms(clk, out);
 
 	always @(posedge clk) begin
 		cnt <= (cnt + 1);
-		if(cnt > 60000) begin // normlanie by bylo 2500000 czyli co 1s
+		if(cnt > 60000) begin // normlanie by bylo ??? czyli co 0.1s
+			out <= !flag; 
+			flag <= !flag;
+			cnt <= 0;
+		end
+	end
+endmodule
+
+module divider_333ms(clk, out);
+	input clk;
+	reg flag = 1;
+	output reg out;
+
+	reg[13:0] cnt = 0;
+
+	always @(posedge clk) begin
+		cnt <= (cnt + 1);
+		if(cnt > 9009) begin // normlanie by bylo 7507 czyli co 0.333s
 			out <= !flag; 
 			flag <= !flag;
 			cnt <= 0;
@@ -149,7 +168,7 @@ module counter(clk, enable, reset, cnt_out);
 
     always @(posedge clk) begin
 
-        if(enable && reset && cnt < 10) begin 
+        if(enable && reset && cnt < 8) begin 
             cnt <= cnt + 1;
             // if(cnt == 10) begin cnt <= 0; end
         end
