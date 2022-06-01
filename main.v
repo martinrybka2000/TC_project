e409d2436144a154836927f5bc5acb833d179448
martinrybka2000 <rybatinek14@gmail.com>
2022-06-01 22:16:39 +0200

    ready
    hotfig 4 changed divider to one module fixed epilespy some refactoring

--- main.v
+++ main.v
@@ -17,26 +17,25 @@ module main(clk_main, switch_inWombat, switch_inDanger, switch_Damaged, switch_I
     wire kabel_Damaged;
     wire kabel_Immobilized;
 
-    wire kabel_I_am_fucked;
+    wire kabel_chuckles_i_m_in_danger;
 
     wire [7:0] kable_LEDs;
 
-    divider_10ms div_10ms   (clk_main, kabel_clk_10ms);
-    divider_333ms div_333ms (clk_main, kabel_clk_333ms);
+    divider div_10ms  (clk_main, 250000,  kabel_clk_10ms); // 0.01 * 50 000 000 / 2
+    divider div_333ms (clk_main, 8325000, kabel_clk_333ms); // 0.333 * 50 000 000 / 2
    
     Debouncer inCombat      (kabel_clk_10ms, switch_inWombat,    kabel_inWombat);
     Debouncer inDanger      (kabel_clk_10ms, switch_inDanger,    kabel_inDanger);
     Debouncer Damaged       (kabel_clk_10ms, switch_Damaged,     kabel_Damaged);
     Debouncer Immobilized   (kabel_clk_10ms, switch_Immobilized, kabel_Immobilized);
 
-    amIfucked doI   (kabel_clk_10ms,    kabel_inDanger,    kabel_Damaged,  kabel_Immobilized, kabel_I_am_fucked);
-    counter selfBoom(kabel_clk_10ms,    kabel_I_am_fucked, kabel_inWombat, kable_LEDs);
-    epilepsy my_eyes(kabel_clk_333ms,   kabel_inWombat,     kable_LEDs,     LEDs);
-
+    chuckles_i_m_in_danger yes   (kabel_clk_10ms,    kabel_inDanger,               kabel_Damaged,  kabel_Immobilized, kabel_chuckles_i_m_in_danger);
+    counter selfBoom             (kabel_clk_10ms,    kabel_chuckles_i_m_in_danger, kabel_inWombat, kable_LEDs);
+    epilepsy my_eyes             (kabel_clk_333ms,   kabel_inWombat,               kable_LEDs,     LEDs);
 
 endmodule
 
-module amIfucked(clk, danger ,damaged, immobilized, i_m_fucked);
+module chuckles_i_m_in_danger(clk, danger ,damaged, immobilized, i_m_fucked);
     input clk;
     input danger;
     input damaged;
@@ -46,10 +45,10 @@ module amIfucked(clk, danger ,damaged, immobilized, i_m_fucked);
     // checking for 2 of 3 inputs
     always @(posedge clk) begin
         if((danger && damaged) || (danger && immobilized) || (damaged && immobilized)) begin
-            i_m_fucked <= 1;
+            i_m_fucked <= 1; // yes
         end
         else begin
-            i_m_fucked <= 0;
+            i_m_fucked <= 0; // no
         end
     end
    
@@ -61,53 +60,37 @@ module epilepsy(clk_3Hz, enable, in_cnt, display);
     input[7:0] in_cnt;
     output reg[7:0] display = 0;
 
-    reg CHADflag <= 0; // celowe blokowanie wszytkich przycisków na rzecz symalacji nie działjącego systemu robota
+    reg CHADflag = 0; // blocking all inputs on purpose to symulate not working robot
 
     always @(posedge clk_3Hz) begin
       if((enable && CHADflag == 0)) begin
-        display <= (8'b11111111 ^ display) & in_cnt;
+        display <= (8'b11111111 ^ display) & in_cnt;    // just displaying
       end
-      else if(in_cnt == 8'b00000000 || CHADflag == 1) begin 
+      else begin
+        display <= 0;  // if robot goes out of combat then reset
+      end
+
+      if(in_cnt == 8'b00000000 || CHADflag == 1) begin  // if dead then light up all LEDs and block changing its state
         CHADflag <= 1;
         display <= 8'b11111111;
       end
-      else begin
-        display <= 0;
-      end
-
+      
     end
 endmodule
 
-module divider_10ms(clk, out);
+module divider(clk, bicycles, out); //just one divider module, reqiure how many cycles to wait
     input clk;
+    input [24:0] bicycles; // defing input and its limitation
+    output out;
+
     reg flag = 0;
-    output out;
-
-    reg[18:0] cnt = 0;
+    reg [24:0] cnt = 0;
 
     assign out = flag;
 
     always @(posedge clk) begin
         cnt <= (cnt + 1);
-        if(cnt > 250000) begin // normlanie by bylo ??? czyli co 0.1s
-            flag <= !flag;
-            cnt <= 0;
-        end
-    end
-endmodule
-
-module divider_333ms(clk, out);
-    input clk;
-    output out;
-    reg flag = 1;
-
-    reg[30:0] cnt = 0;
-
-    assign out = flag;
-    
-    always @(posedge clk) begin
-        cnt <= (cnt + 1);
-        if(cnt > 4162500) begin // normlanie by bylo 8333333 czyli co 0.333s
+        if(cnt > bicycles) begin
             flag <= !flag;
             cnt <= 0;
         end
@@ -160,17 +143,15 @@ module counter(clk, enable, reset, cnt_out);
 
     always @(posedge clk) begin
 
-        if(enable && reset && cnt > 0) begin  // couting to 10 sec
+        if(enable && reset && cnt > 0) begin  // couting from 8s to 0s
             cnt1s <= cnt1s + 1;
-            if(cnt1s >= 100) begin
-                // cnt <= cnt + 1;
-                // cnt <= cnt << 1;
+            if(cnt1s >= 100) begin  // couting to 1s from the 10ms timer
                 cnt <= cnt >> 1;
                 cnt1s <= 0;
             end
         end
 
-        if(!reset) begin
+        if(!reset) begin // reseting the counter
             cnt <= 255;
             cnt1s <= 0;
         end
